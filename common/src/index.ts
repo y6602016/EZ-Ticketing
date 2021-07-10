cd28062f8c014e6e714b7e5afd2dffb4d3830f59
mike <y6602016@gmail.com>
2021-07-10 08:52:24 +0800

    config

---
+++ common/src/index.ts
@@ -0,0 +1,14 @@
+interface Color {
+  red: number;
+  blue: number;
+  green: number;
+}
+
+const color: Color = {
+  red: 10,
+  blue: 12,
+  green: 12,
+};
+console.log(color);
+
+export default color;
