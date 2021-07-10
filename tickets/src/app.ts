8b35ab5082dc262f9831c73c8309005db8052b8b
mike <y6602016@gmail.com>
2021-07-10 17:28:09 +0800

    find ticket

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -4,6 +4,7 @@ import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@miketickets/common";
 
 import { createTicketRouter } from "./routes/new";
+import { showTicketRouter } from "./routes/show";
 
 const app = express();
 app.use(express.json());
@@ -16,6 +17,7 @@ app.use(
 );
 app.use(currentUser);
 app.use(createTicketRouter);
+app.use(showTicketRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
---
+++ tickets/src/routes/show.ts
@@ -0,0 +1,20 @@
+import express, { request, Request, Response } from "express";
+import { body } from "express-validator";
+
+import { NotFoundError } from "@miketickets/common";
+import { requireAuth, validateRequest } from "@miketickets/common";
+import { Ticket } from "../models/ticket";
+
+const router = express.Router();
+
+router.get("/api/tickets/:id", async (req: Request, res: Response) => {
+  const ticket = await Ticket.findById(req.params.id);
+
+  if (!ticket) {
+    throw new NotFoundError();
+  }
+
+  res.send(ticket);
+});
+
+export { router as showTicketRouter };
