48f93c0cb3e476226d58a6b2a506c72310e541cd
mike <y6602016@gmail.com>
2021-07-16 23:34:28 +0800

    payment complete

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -12,6 +12,8 @@ import {
 import { stripe } from "../stripe";
 import { Order } from "../models/order";
 import { Payment } from "../models/payment";
+import { PaymentCreatedPublisher } from "../events/publisher/payment-created-publisher";
+import { natsWrapper } from "../nats-wrapper";
 
 const router = express.Router();
 
@@ -49,7 +51,13 @@ router.post(
 
     await payment.save();
 
-    res.status(201).send({ success: true });
+    await new PaymentCreatedPublisher(natsWrapper.client).publish({
+      id: payment.id,
+      orderId: payment.orderId,
+      stripeId: payment.stripeId,
+    });
+
+    res.status(201).send({ id: payment.id });
   }
 );
 
